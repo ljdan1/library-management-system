95d13e96018b20218ff91f9504705992d8af3e51
Daniel F <166423141+ljdan1@users.noreply.github.com>
2024-07-09 17:56:19 +0300

    Create testfile.py

---
+++ .github/workflows/testfile.py
@@ -0,0 +1,19 @@
+import pytest
+from library_management import add_book, search, edit, remove
+
+
+def test_add_book():
+
+    pass
+
+def test_search():
+
+    pass
+
+def test_edit():
+n
+    pass
+
+def test_remove():
+
+    pass
